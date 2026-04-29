ca9cbc846f82efe759944cd146bf1109959f23b4
shruti0101 <shrutiguptacu@gmail.com>
2026-04-29 12:12:01 +0530

    changes

--- Component/Landingpage/Home.jsx
+++ Component/Landingpage/Home.jsx
@@ -25,11 +25,11 @@ export default function Hero() {
     <>
       <section className="w-full h-full bg-[#f7f7f7]">
         {/* HERO SECTION */}
-        <div className="mt-3">
+        <div className="">
           <div className="">
             {/* LEFT BANNER */}
             <div className="relative h-[260px] md:h-[320px] lg:h-[80vh] overflow-hidden shadow-md">
-              
+
               <Swiper
                 modules={[Navigation, Pagination, Autoplay]}
                 navigation={{
