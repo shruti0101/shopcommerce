e6e94a55ebd633dd5ad096d63c2e496d148a8caa
shruti0101 <shrutiguptacu@gmail.com>
2026-05-28 15:42:23 +0530

    changes

--- app/product/[slug]/ProductView.jsx
+++ app/product/[slug]/ProductView.jsx
@@ -625,9 +625,15 @@ const selectedSizeData =
     </button>
 
     {/* WHATSAPP */}
-    <a href="tel:+918130385561" className="h-[62px] rounded-full border border-[#25D366] bg-[#25D366] text-white font-medium transition-all duration-300 hover:scale-[1.02] hover:bg-[#1ebe5d]">
+
+<a href="tel:+918130385561">
+
+
+    <button  className="h-[62px] rounded-full border border-[#25D366] bg-[#25D366] text-white font-medium transition-all duration-300 hover:scale-[1.02] hover:bg-[#1ebe5d]">
       WhatsApp Now
-    </a>
+    </button>
+</a>
+
 
     {/* WISHLIST */}
     <button
